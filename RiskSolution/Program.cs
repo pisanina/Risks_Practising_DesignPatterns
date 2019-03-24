f131a13d5a8f14a5acc6210c97586288db249cfe
Pisanina <mailszarookiego@o2.pl>
2019-03-24 13:04:12 +0100

    Add project files.

---
+++ RiskSolution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RiskSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
